7d3b4fd3e7d2a8d71e9d0c851956518989fa03de
Michael Brenndoerfer <mbnet@hotmail.de>
2016-02-02 10:23:07 +0100

    Initial Setup

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `This is the Angular-2-Quickstarter`
+})
+
+export class AppComponent {
+
+}
